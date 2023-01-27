4043d51a671891d736c7feae9fdbfd147f5b30a6
Dan Ha <smha118@gmail.com>
2023-01-26 16:53:47 -0800

    Rscript

--- Part2/Part2.R
+++ Part2/Part2.R
@@ -66,13 +66,16 @@ for(i in 1:nrow(CountyPop)){
 # Day 3.2: Save & load data
 # =====================================================================
 
-
+library(dplyr)
+library(ggplot2)
 #There are mainly 3 different ways you can save your data.
 #If you are saving data.frame, you can use simple write.table function
 CountyPopFilter <- select(CountyPop, County, Population, Per10K)
+
 write.table(CountyPopFilter,file="CountyPopfilter.csv",sep=",",row.names = F)
 
 #We know how to load the table - read.table or read.csv
+rm(CountyPopFilter)
 rm(CountyPopFilter)
 CountyPopFilter<-read.table("CountyPopfilter.csv",sep=",",header=T)
 
@@ -86,7 +89,11 @@ CountyPopFilter <- select(CountyPop, County, Population, Per10K)
 
 VaxDataMerged <- left_join(CountyPopFilter, CountyVaxDataCA, by = c("County"="county"))
 
-NormalizedVaxData <- VaxDataMerged%>% mutate(VaxDensity = fully_vaccinated/Per10K) %>% filter(VaxDensity > 10)%>% filter(!is.na(VaxDensity))
+
+VaxDataMerged$Population <- as.numeric(gsub(",","",VaxDataMerged$Population))
+
+NormalizedVaxData <- VaxDataMerged %>% mutate(VaxDensity = fully_vaccinated/Per10K) %>% filter(VaxDensity > 10)%>% filter(!is.na(VaxDensity)) %>%
+mutate(Population_category=ifelse(Population > 1000000, "high","low"))
 
 VaccinationRate <- NormalizedVaxData %>%
   select(date, County, VaxDensity, fully_vaccinated, Per10K, at_least_one_dose, new_doses_administered) %>%
@@ -96,9 +103,17 @@ figure <- ggplot(VaccinationRate, aes(x = ymd(date), y = fully_vaccinated, color
   labs(x = "Date", 
        y = "Fully Vaccinated (Total)", 
        title = "Vaccinations in Large Counties")
-
+figure
 #Save any object by saveRDS
 saveRDS(figure,file="figure.rds")
+rm(figure)
+figure<-readRDS("figure.rds")
+
+png(filename = "geom_smmooth_ex.png",width = 8,height=8,units = "in",res=300)
+figure
+dev.off()
+
+
 #or save multiple objects like this
 rm(figure)
 figure
@@ -113,7 +128,7 @@ load("part2.Rdata")
 
 
 
-?kable
+
 # =====================================================================
 # Day 3.3: R markdown
 # =====================================================================
